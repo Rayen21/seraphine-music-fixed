c80a861aba2af02831a0106afbac6a51161c6094
hanqing ren <hanqingren@hanqingdeMacBook-Pro.local>
2026-09-14 14:00:57 +0800

    fix: add missing quotes in build.rs println! macro

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,7 +2,7 @@ fn main() {
   tauri_build::build();
   
   #[cfg(target_os = "macos")]
-  println!(cargo:rustc-link-lib=framework=CoreServices);
+  println!("cargo:rustc-link-lib=framework=CoreServices");
 
   // CGEventTapCreate requires CoreGraphics
   #[cfg(target_os = "macos")]
